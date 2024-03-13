ba3790f07c84f315c2f9026f2ad9e10b6954be66
Han Yan <yh20020606@gmail.com>
2024-03-13 17:06:47 -0400

    first draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,66 @@
-import Image from "next/image";
+import type { NextPage } from 'next';
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <header className="bg-white shadow-md py-6">
+        <div className="container mx-auto px-6">
+          <h1 className="text-3xl font-bold text-center text-gray-900">Han Yan</h1>
+          <p className="text-center text-gray-600">805-905-5791 | fqw6pv@virginia.edu</p>
+          <p className="text-center"><a href="https://www.linkedin.com/in/hanyan0606" className="text-blue-500">LinkedIn Profile</a></p>
         </div>
-      </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main className="container mx-auto px-6 flex-grow">
+        <section className="my-12">
+          <h2 className="text-2xl font-bold text-gray-700">Education</h2>
+          <p className="text-gray-600 mt-2">University of Virginia, McIntire School of Commerce - B.S. in Commerce (Quantitative Analysis), double major in Computer Science, minor in Data Science, GPA: 3.7/4.0, Expected May 2025</p>
+        </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <section className="my-12">
+          <h2 className="text-2xl font-bold text-gray-700">Technical Skills</h2>
+          <ul className="list-disc list-inside text-gray-600">
+            <li>Programming: Java, C, React, Node.js, AWS, SQLite, Docker, Shell, C++, Python, SQL, Linux, R, JavaScript</li>
+            <li>Business: Tableau, Financial Accounting, Managerial Accounting</li>
+          </ul>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <section className="my-12">
+          <h2 className="text-2xl font-bold text-gray-700">Experience</h2>
+          <div className="text-gray-600">
+            <h3 className="font-bold">Mastercard - Infrastructure Software Engineer Intern</h3>
+            <p className="italic">June 2023–August 2023</p>
+            <ul className="list-disc list-inside">
+              <li>Initiated a project to transition mainframe development to modern DevOps with Zowe CLI, Node.js, and Jenkins API.</li>
+              <li>Managed Ansible Tower docker image in a zCX with CentOS environment.</li>
+              <li>Co-developed full stack Angular project using TypeScript and JavaScript.</li>
+            </ul>
+          </div>
+        </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <section className="my-12">
+          <h2 className="text-2xl font-bold text-gray-700">Projects</h2>
+          <div className="text-gray-600">
+            <h3 className="font-bold">Java Course Registration System - Class Project</h3>
+            <ul className="list-disc list-inside">
+              <li>Developed a command-line User Interface (UI) course review application using Java, Gradle, and SQLite.</li>
+              <li>Implemented a robust Three-Layer Architecture for system resilience.</li>
+            </ul>
+          </div>
+          <div className="text-gray-600 mt-6">
+            <h3 className="font-bold">Don't Mess with the Cat - Personal Web Game</h3>
+            <p>Developed a web game with JavaScript, published online and gained 152 plays within the school. <a href="https://codehs.com/sandbox/williamyan/dont-mess-with-the-cat" className="text-blue-500">Play the Game</a></p>
+          </div>
+        </section>
+      </main>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer className="bg-white shadow-md py-6 mt-auto">
+        <div className="container mx-auto px-6 text-center text-gray-600">
+          &copy; {new Date().getFullYear()} Han Yan.
+        </div>
+      </footer>
+    </div>
   );
-}
+};
+
+export default Home;
